2cc654da394ca64d8630a74eb340aca55c399339
Claireese <claireese.underwood@gmail.com>
2023-10-08 17:51:44 -0500

    I altered a tremendous amount. It's almost fully functional

--- todo_manager.js
+++ todo_manager.js
@@ -9,48 +9,53 @@ task = {};
 
 // adds a task to the list
 const taskAdd = (response) => {
-    task[taskList][taskName] = [userInput]
-    task[taskList][taskName][taskStatus] = false
-    return task[taskList][taskName][taskStatus]
+    task[response] = false
+    console.log(Object.keys(task))
 }
 
 // marking task as complete (toggle true/false)
 const taskComplete = (response) => {
-    task[taskList][userInput][taskStatus] = true
-    console.log("'"+ userInput + "' has been completed.\n\n")
-    return task[taskList][taskName][taskStatus]
+    task[response] = true
+    console.log("'"+ response + "' has been completed.\n\n")
+    delete task[response]
 }
 
 // removing the task
 const taskRemove = (response) => {
-    delete task[taskList][userInput]
+    delete task[response]
 }
 
 // editing a task
 const taskEdit = (response) => {
-    return "done"
+    let edit = "";
+    delete task[response];
+    edit = prompt("And what would you like to change it to? \n");
+    task[edit] = false;
+    console.log(`You changed ${response} to ${edit}\n\n`)
+    
 }
 
 // view the task
 const viewList = () => {
-    return Object.keys(task)
+    console.log(Object.keys(task));
+    return Object.keys(task);
 }
 
 const promptOptions = () => {
      switch (response = prompt("Please enter a number. ")) {
         case '1':
-            taskAdd(response = prompt("Please enter the task you want to add. "));
-            console.log("You added: "+response);
+            taskAdd(response = prompt("Please enter the task you want to add: "));
+            console.log("You added: "+response"\n\n");
             break;
 
         case '2':
-            taskEdit();
+            taskEdit(response = prompt("Please enter the task you want to edit: "));
             break;
 
         case '3':
-            console.log(task.keys)
+            console.log(Object.keys(task))
             taskComplete(response = prompt("Please enter the task you want to complete: "));
-            console.log("Task Complete! "+response+" \n\n\n\n");
+            console.log("Task Complete! "+response+"\n\n");
             break;
 
         case '4':
@@ -73,16 +78,30 @@ const promptOptions = () => {
 const toDoListShell = () => {
     let response = "";
     let runProgram = true;
+    let count = 0;
     while (runProgram === true){
-        if (task.keys === undefined){
-            console.log(`${name}'s ToDo List\n\n No Tasks Yet!\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
-            promptOptions()
+        console.log(`Your ToDo List\n ${Object.keys(task)}\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
+        promptOptions();
+        if (count === 5){
+            runProgram === false;
         }
-        else if (task.keys !== undefined){
-            console.log(`Your ToDo List\n ${task.keys.values}\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
-            promptOptions()
-        }
+        count++;
+        // console.log(task.keys)
+        // if (count === 0){
+        //     count = 1;
+        //     console.log(`${name}'s ToDo List\n\n No Tasks Yet!\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
+        //     promptOptions()
+        //     console.log("boop")
+        // }
+        // else if (count === 1){
+        //     console.log(`Your ToDo List\n ${task.keys}\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
+        //     promptOptions()
+        //     console.log(task.keys + "task")
+        //     continue;
+        // }
+        // console.log("check this point")
     }
+    return "Done."
 }
 //     //let selection = prompt("Enter 1: Add a task \n Enter 2: Modify \n Enter 3: Delete\n Enter 4: View List")
 //     const readline = require("readline");
