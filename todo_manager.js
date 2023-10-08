b1bf72b964d1ada1673b9bdf4c699264b1f26de3
Claireese <claireese.underwood@gmail.com>
2023-10-07 20:58:15 -0500

    added more functionality to the toDoListShell. It is still untested

--- todo_manager.js
+++ todo_manager.js
@@ -27,28 +27,54 @@ const viewList = () => {
 
 //prompt for userInput
 const toDoListShell = () => {
-    //let selection = prompt("Enter 1: Add a task \n Enter 2: Modify \n Enter 3: Delete\n Enter 4: View List")
-    const readline = require("readline");
+    let response = "";
+    while (runProgram === true){
+        console.log(`Your ToDo List\n ${task.keys}\n\n______________________\n What would you like to do?\n (1) Add Task\n (2) Edit Task\n (3) Complete Task\n (4) Remove Task\n (5) Close Program`)
+        switch (response = prompt("Please enter a number.")) {
+            case 1:
+                taskAdd(response = prompt("Please enter the task you want to add."));
+                console.log("You added: "+response);
+                break;
+            case 2:
+                break;
+            case 3:
+                console.log(task.keys)
+                taskComplete(response = prompt("Please enter the task you want to complete"));
+                console.log("Task Complete! "+response);
+                break;
+            case 4:
+                break;
+            case 5:
+                runProgram = false;
+                console.log("Goodbye!")
+                break;
+            default:
+                console.log("Oops! Invalid Input")
+                break;
+        }
+    }
+//     //let selection = prompt("Enter 1: Add a task \n Enter 2: Modify \n Enter 3: Delete\n Enter 4: View List")
+//     const readline = require("readline");
 
-    // create interface for input and output
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
+//     // create interface for input and output
+//     const rl = readline.createInterface({
+//       input: process.stdin,
+//       output: process.stdout,
+//     });
     
-    // create empty user input
-    let userInput = "";
+//     // create empty user input
+//     let userInput = "";
     
-    // question user to enter name
-    rl.question("What is your name\n", function (string) {
-      userInput = string;
+//     // question user to enter name
+//     rl.question("What is your name\n", function (string) {
+//       userInput = string;
     
-      console.log("Your name is " + userInput);
+//       console.log("Your name is " + userInput);
     
-      // close input stream
-      rl.close();
+//       // close input stream
+//       rl.close();
 
-    });
+//     });
     // if (selection === '1'){
     //    let taskName = prompt("Enter a task");
     //    taskAdd(taskName)
